790be568153671f9984612e4ae2846c033560e28
Álvaro Sedano López <asedano85@gmail.com>
2021-07-15 15:06:50 +0200

    First code version

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            OnView.Visibility = Visibility.Hidden;
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            OnView.Visibility = Visibility.Visible;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            OnView.Visibility = Visibility.Visible;
+        }
+    }
+}
